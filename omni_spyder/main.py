14d22b3fcb2ad54d89f6626e1207bf697a945066
zhutianyu <zhutianyu@xiaomi.com>
2025-09-22 10:56:56 +0800

    修改测试结过果获取

--- omni_spyder/main.py
+++ omni_spyder/main.py
@@ -3,12 +3,12 @@ import spyder_links
 import spyder_results
 
 if input('执行任务链接抓取\n确认无误后输入Yes') == 'y':
-    with open('task.txt', 'a+') as f_task:
+    with open('task.txt', 'a+', encoding='latin-1') as f_task:
         f_task.truncate(0)
         spyder_links.catch_links(f_task)
 
 time.sleep(1)
 if input('执行任务结果抓取\n确认无误后输入Yes') == 'y':
-    with open('result.txt', 'a+') as f_result:
+    with open('result.txt', 'a+', encoding='latin-1') as f_result:
         f_result.truncate(0)
         spyder_results.catch_result(f_result)
